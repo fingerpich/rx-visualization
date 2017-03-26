a6b69ecae8eed803cdd15dd6514839bc701c7140
Angular CLI <angular-cli@angular.io>
2017-03-26 23:14:10 +0430

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { RxstudioPage } from './app.po';
+
+describe('rxstudio App', () => {
+  let page: RxstudioPage;
+
+  beforeEach(() => {
+    page = new RxstudioPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
