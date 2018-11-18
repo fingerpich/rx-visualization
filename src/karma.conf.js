a934711444efb22da59d98b7d671723ba0959aa0
fingerpich <zarei.bs@gmail.com>
2018-11-18 04:16:21 +0330

    update readme

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,18 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222',
+        ]
+      }
+    },
+    browsers: ['ChromeHeadless'],
+    singleRun: true
   });
 };
