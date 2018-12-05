526007613f3099a23d00b25854ae6c80f5180527
fingerpich <zarei.bs@gmail.com>
2018-12-05 11:48:33 +0330

    updated packages and fix a problem with it

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -72,6 +72,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+import 'reflect-metadata';
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
